125f3e1eb304176a098376033a7e77589f75ef74
caladdd <juanpablo.calad@gmail.com>
2020-02-25 11:57:46 -0500

    add sign out

--- authentication/controllers/user.js
+++ authentication/controllers/user.js
@@ -43,3 +43,8 @@ exports.signIn = (req, res) => {
     })
 
 };
+
+exports.signOut = (req, res) => {
+    res.clearCookie("t");
+    return res.status(200).json({message: "Sign out success!"});
+};
--- authentication/routes/user.js
+++ authentication/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signUp, signIn } = require('../controllers/user');
+const { signUp, signIn, signOut } = require('../controllers/user');
 const { userSignUpValidator } = require('../validator/validator');
 
 const router = express.Router();
 
 router.post("/signup", userSignUpValidator, signUp);
 router.post("/signin", signIn);
+// signOut
+router.get("/signout", signOut);
 
 module.exports = router;
