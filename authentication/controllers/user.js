14588982232d0325d3c6af4af2bf4cc7372d3293
caladdd <juanpablo.calad@gmail.com>
2020-02-26 10:20:12 -0500

    update user

--- authentication/controllers/user.js
+++ authentication/controllers/user.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const User = require('../models/user');
 
 exports.userById = (req, res, next, id) => {
@@ -15,26 +16,43 @@ exports.userById = (req, res, next, id) => {
 
 exports.hasAuthorization = (req, res, next) => {
     const authorized = req.profile && req.auth && req.profile._id === req.auth._id;
-    if(!authorized){
+    if (!authorized) {
         return res.status(403).json({
             error: "User is not authorized to perform this action"
         })
     }
 };
 
-exports.allUsers = (req, res) =>{
-    User.find((err, users)=>{
-        if(err){
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
             return res.status(400).json({
-                 error: err
+                error: err
             })
         }
         res.status(200).json({users});
     }).select("name email updated created")
 };
 
-exports.getUser = (req, res) =>{
+exports.getUser = (req, res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
 };
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile;
+    user = _.extend(user, req.body); // extend -- mutate the source object
+    user.updated = Date.now();
+    user.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: "You are not authorized to perform this action"
+            })
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({user})
+    })
+};
+
--- authentication/routes/user.js
+++ authentication/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {userById, allUsers, getUser} = require('../controllers/user');
+const {userById, allUsers, getUser, updateUser} = require('../controllers/user');
 const {requireSignIn} = require('../controllers/auth');
 
 const router = express.Router();
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignIn, getUser);
+router.put("/user/:userId", requireSignIn, updateUser);
 
 // any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
