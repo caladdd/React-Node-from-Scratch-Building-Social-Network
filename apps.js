8e8654459c4a4a2924e0c55a7208443502434209
caladdd <juanpablo.calad@gmail.com>
2020-03-04 19:38:30 -0500

    add cors

--- apps.js
+++ apps.js
@@ -1,42 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
 //db
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}).then(() => {
-    console.log("DB Connected")
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+  console.log("DB Connected");
 });
 
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`)
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error: ${err.message}`);
 });
 
 //bring  in routes
-const PostRoutes = require('./posts/routes/post');
-const authRoutes = require('./authentication/routes/auth');
-const userRoutes = require('./authentication/routes/user');
+const PostRoutes = require("./posts/routes/post");
+const authRoutes = require("./authentication/routes/auth");
+const userRoutes = require("./authentication/routes/user");
 
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
-app.use('/', PostRoutes);
-app.use('/', authRoutes);
-app.use('/', userRoutes);
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({ error: 'Unauthorized!'});
-    }
+app.use(cors());
+app.use("/", PostRoutes);
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized!" });
+  }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`node api is listened on port: ${port}`)
+  console.log(`node api is listened on port: ${port}`);
 });
